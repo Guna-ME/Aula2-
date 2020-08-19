9ae736c01ce90f560aae0b26da53109dab6104f9
Guna-ME <Dudamarcos151@hotmail.com>
2020-08-19 17:16:11 -0300

    :rocket: Primeiros passos PY

---
+++ Aula2.py
@@ -0,0 +1,34 @@
+
+# Mostra algo na tela para o usuário;
+print("Vamos começar")
+
+# Para fazer a declaração de váriaveis não é necessário colocar o tipo dela;
+numeroInteiro   = 7
+numeroDecimal   = 7.42566
+palavra         = "Maria Eduarda"
+verdadeiroFalso = True
+
+# Mostra na tela os valores armazenados nas variáveis;
+print(type(numeroInteiro))
+print(type(numeroDecimal))
+print(type(palavra))
+print(type(verdadeiroFalso))
+
+# Mostra o tamanho da variavel tipo string;
+print(len(palavra))
+
+# Quando adiciona o * mais um número, ele vai mostrar o número de vezes que você pediu;
+print(palavra*7)
+
+# Concatenando(somando,juntando) Strings;
+print(palavra + "", palavra)
+
+# Arredondar números decimais(float);
+print(round(numeroDecimal,3))
+print("%.2f" % (numeroDecimal))
+
+
+
+
+
+
